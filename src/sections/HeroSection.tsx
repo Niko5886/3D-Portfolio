b795e291e43822e79278bea0c584c2498896e074
Nikolay Stoyanov <lobido1988@gmail.com>
2026-08-25 21:16:56 +0300

    style: reduce hero avatar size by 10%

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -76,7 +76,7 @@ export default function HeroSection() {
       </div>
 
       {/* Portrait */}
-      <div className="absolute left-1/2 -translate-x-1/2 z-10 top-1/2 -translate-y-1/2 sm:top-auto sm:translate-y-0 sm:bottom-0 w-[280px] sm:w-[360px] md:w-[440px] lg:w-[520px]">
+      <div className="absolute left-1/2 -translate-x-1/2 z-10 top-1/2 -translate-y-1/2 sm:top-auto sm:translate-y-0 sm:bottom-0 w-[252px] sm:w-[324px] md:w-[396px] lg:w-[468px]">
         <FadeIn delay={0.6} y={30}>
           <Magnet
             padding={150}
